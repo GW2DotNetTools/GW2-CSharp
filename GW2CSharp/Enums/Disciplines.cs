da6b93bebba9437b145043c01361b7f595b8482d
Dreanor <mikeundead@googlemail.com>
2015-09-17 20:52:19 +0200

    added disciplines

---
+++ GW2CSharp/Enums/Disciplines.cs
@@ -0,0 +1,42 @@
+﻿
+namespace GW2CSharp.Enums
+{
+    /// <summary>
+    /// All crafting disciplines in GW2
+    /// </summary>
+    public enum Disciplines
+    {
+        /// <summary>
+        /// Artificer.
+        /// </summary>
+        Artificer,
+        /// <summary>
+        /// Armorsmith.
+        /// </summary>
+        Armorsmith,
+        /// <summary>
+        /// Chef.
+        /// </summary>
+        Chef,
+        /// <summary>
+        /// Huntsman.
+        /// </summary>
+        Huntsman,
+        /// <summary>
+        /// Jeweler.
+        /// </summary>
+        Jeweler,
+        /// <summary>
+        /// Leatherworker.
+        /// </summary>
+        Leatherworker,
+        /// <summary>
+        /// Tailor.
+        /// </summary>
+        Tailor,
+        /// <summary>
+        /// Weaponsmith.
+        /// </summary>
+        Weaponsmith
+    }
+}
