14557214fe19b1fce16fdef6ed2ab7cfd64bcf45
Michael Stephenson <m.k.stephenson@outlook.com>
2015-11-20 23:37:01 +0100

    Implemented minis endpoint

---
+++ GW2CSharp/V2/Unauthenticated/Minis/Mini.cs
@@ -0,0 +1,69 @@
+﻿using GW2CSharp.Services;
+using System.Drawing;
+
+namespace GW2CSharp.V2.Unauthenticated.Minis
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Mini
+	{
+		/// <summary>
+		/// The mini id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// The mini name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// A description of how to unlock the mini (only present on a few entries).
+		/// </summary>
+		public string Unlock { get; set; }
+
+		/// <summary>
+		/// The mini icon URL.
+		/// </summary>
+		public string Icon { get; set; }
+
+		/// <summary>
+		/// The sort order that is used for displaying the mini in-game.
+		/// </summary>
+		public int Order { get; set; }
+
+		/// <summary>
+		/// The item which unlocks the mini and can be resolved against /v2/items.
+		/// </summary>
+		public int ItemId { get; set; }
+
+		/// <summary>
+		/// Initialize the new mini object
+		/// </summary>
+		/// <param name="id">The mini id.</param>
+		/// <param name="name">The mini name.</param>
+		/// <param name="unlock">A description of how to unlock the mini (only present on a few entries).</param>
+		/// <param name="icon">The mini icon URL.</param>
+		/// <param name="order">The sort order that is used for displaying the mini in-game.</param>
+		/// <param name="item_id">The item which unlocks the mini and can be resolved against /v2/items.</param>
+		public Mini(int id, string name, string unlock, string icon, int order, int item_id)
+		{
+			Id = id;
+			Name = name;
+			Unlock = unlock;
+			Icon = icon;
+			Order = order;
+			ItemId = item_id;
+		}
+
+		/// <summary>
+		/// Returns the icon for the mini as a Bitmap
+		/// </summary>
+		/// <returns>Mini icon as a Bitmap</returns>
+		public Bitmap GetImage()
+		{
+			return RenderService.GetImage(Icon);
+		}
+	}
+}
---
+++ GW2CSharp/V2/Unauthenticated/Minis/MinisWrapper.cs
@@ -0,0 +1,39 @@
+﻿using GW2CSharp.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace GW2CSharp.V2.Unauthenticated.Minis
+{
+	/// <summary>
+	/// Represents a wrapper for the v2/minis endpoint.
+	/// </summary>
+	public class MinisWrapper : ApiBase
+	{
+		/// <summary>
+		/// Returns a single mini that matches the id.
+		/// </summary>
+		/// <param name="id">The mini identifier.</param>
+		/// <param name="requestedLanguage"></param>
+		/// <returns>Mini that matches the id.</returns>
+		public Mini Get(int id, RequestedLanguage requestedLanguage = RequestedLanguage.En)
+		{
+			return DeserializeObject<Mini>(string.Format("https://api.guildwars2.com/v2/minis/{0}", id));
+		}
+
+		/// <summary>
+		/// Returns all known minis.
+		/// </summary>
+		/// <returns>All the known minis.</returns>
+		public Dictionary<int, Lazy<Mini>> GetAll()
+		{
+			List<int> miniIds = DeserializeObject<List<int>>("https://api.guildwars2.com/v2/minis");
+
+			var minis = new Dictionary<int, Lazy<Mini>>();
+			foreach (var miniId in miniIds)
+			{
+				minis.Add(miniId, new Lazy<Mini>(() => Get(miniId)));
+			}
+			return minis
+        }
+	}
+}
