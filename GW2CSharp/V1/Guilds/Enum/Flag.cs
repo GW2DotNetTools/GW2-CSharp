0140982a71d61a2caca5753b75ccd339f39c49b9
Michael Stephenson <m.k.stephenson@outlook.com>
2015-11-22 00:13:52 +0100

    Start of v1 guild information implementation, still needs documentation and proper endpoints

---
+++ GW2CSharp/V1/Guilds/Enum/Flag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2CSharp.V1.Guilds.Enum
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public enum Flag
+	{
+		/// <summary>
+		/// Undefined
+		/// </summary>
+		Undefined,
+
+		/// <summary>
+		/// Flip the background horizontally
+		/// </summary>
+		FlipBackgroundHorizontal,
+
+		/// <summary>
+		/// Flip the background vertically
+		/// </summary>
+		FlipBackgroundVertical,
+
+		/// <summary>
+		/// Flip the foreground horizontally
+		/// </summary>
+		FlipForegroundHorizontal,
+
+		/// <summary>
+		/// Flip the foreground vertically
+		/// </summary>
+		FlipForegroundVertical
+	}
+}
---
+++ GW2CSharp/V1/Guilds/Guild.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2CSharp.V1.Guilds
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Guild
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public string GuildId { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string GuildName { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Tag { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Emblem Emblem { get; set; }
+	}
+}
