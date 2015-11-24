381eb8155f83c4ac9fa11261b27b75b8950da2b6
Dreanor <mikeundead@googlemail.com>
2015-11-24 22:49:01 +0100

    unit tested listing endpoint

--- GW2CSharp/V2/Unauthenticated/Commerce/Listing.cs
+++ GW2CSharp/V2/Unauthenticated/Commerce/Listing.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 namespace GW2CSharp.V2.Unauthenticated.Commerce
 {
     /// <summary>
-    /// 
+    /// Represents a single Listing object.
     /// </summary>
     public class Listing
     {
         /// <summary>
-        /// 
+        /// Initialization of the Price object. 
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="buys"></param>
-        /// <param name="sells"></param>
+        /// <param name="id">The item id.</param>
+        /// <param name="buys">A list of all buy listings, ascending from lowest buy order.</param>
+        /// <param name="sells">A list of all sell listings, ascending from lowest sell offer.</param>
         public Listing(int id, IEnumerable<TpItemListing> buys, IEnumerable<TpItemListing> sells)
         {
             Item = new ItemWrapper().GetById(id);
--- GW2CSharp/V2/Unauthenticated/Commerce/ListingWrapper.cs
+++ GW2CSharp/V2/Unauthenticated/Commerce/ListingWrapper.cs
@@ -8,24 +8,24 @@ using System.Threading.Tasks;
 namespace GW2CSharp.V2.Unauthenticated.Commerce
 {
     /// <summary>
-    /// 
+    /// Represents a wrapper for the v2/listings endpoint.
     /// </summary>
     public class ListingWrapper : ApiBase
     {
         /// <summary>
-        /// 
+        /// Returns a buy and sell listings of a given item id from the trading post.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">Item id</param>
+        /// <returns>Buy and sell listings of a given item id.</returns>
         public Listing GetById(int id)
         {
             return DeserializeObject<Listing>(string.Format("https://api.guildwars2.com/v2/commerce/listings/{0}", id));
         }
 
         /// <summary>
-        /// 
+        /// Returns all buy and sell listing information from the trading post.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>All buy and sell listing informations.</returns>
         public Dictionary<int, Lazy<Listing>> GetAll()
         {
             List<int> itemIds = DeserializeObject<List<int>>(string.Format("https://api.guildwars2.com/v2/commerce/listings"));
@@ -40,21 +40,21 @@ namespace GW2CSharp.V2.Unauthenticated.Commerce
         }
 
         /// <summary>
-        /// 
+        /// Returns multiple buy and sell listing information from the trading post.
         /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
-        public Dictionary<int, Lazy<Listing>> GetMultiple(params int[] ids)
+        /// <param name="ids">Item id's</param>
+        /// <returns>Multiple buy and sell listing informations.</returns>
+        public IEnumerable<Listing> GetMultiple(params int[] ids)
         {
             return GetMultiple(new List<int>(ids));
         }
 
         /// <summary>
-        /// 
+        /// Returns multiple buy and sell listing information from the trading post.
         /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
-        public Dictionary<int, Lazy<Listing>> GetMultiple(IEnumerable<int> ids)
+        /// <param name="ids">Item id's</param>
+        /// <returns>Multiple buy and sell listing informations.</returns>
+        public IEnumerable<Listing> GetMultiple(IEnumerable<int> ids)
         {
             string url = "https://api.guildwars2.com/v2/commerce/listings?ids=";
             foreach (var id in ids)
@@ -62,7 +62,7 @@ namespace GW2CSharp.V2.Unauthenticated.Commerce
                 url += id + ",";
             }
 
-            return DeserializeObject<Dictionary<int, Lazy<Listing>>>(url);
+            return DeserializeObject<IEnumerable<Listing>>(url);
         }
     }
 }
