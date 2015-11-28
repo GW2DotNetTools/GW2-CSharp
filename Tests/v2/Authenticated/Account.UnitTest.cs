be534f4aeed9b35962b8e676ec898b121a96886e
Dreanor <mikeundead@googlemail.com>
2015-11-28 21:06:41 +0100

    changed test to run with any cultureinfo

--- Tests/v2/Authenticated/Account.UnitTest.cs
+++ Tests/v2/Authenticated/Account.UnitTest.cs
@@ -15,7 +15,7 @@ namespace Tests.V2.Authenticated
             Account account = GW2Api.V2.Account.Get("D0F26FAD-FAC1-E649-A177-CFEC0297C5B49AF875D9-CFC7-434B-8FF1-DC11D0B4A65D");
 
             Assert.AreEqual(account.Access, AccountType.GuildWars2);
-            Assert.AreEqual(account.Created, DateTime.Parse("05.08.2015 21:51:00"));
+            Assert.AreEqual(account.Created, new DateTime(2015, 8, 5, 21, 51, 00));
             Assert.GreaterOrEqual(account.FractalLevel, 7);
             Assert.GreaterOrEqual(account.Guilds.Count(), 1);
             Assert.GreaterOrEqual(account.Id, Guid.Parse("907e7008bc3be511a22a78e7d1936ef0"));
